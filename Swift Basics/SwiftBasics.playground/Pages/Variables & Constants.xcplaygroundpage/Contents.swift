93b021d4ff10c18f99afebcd2d9d48785dca6c00
Michael <Michael.Bardwell.Scott@Gmail.com>
2017-11-17 10:56:15 -0500

    Types

---
+++ Swift Basics/SwiftBasics.playground/Pages/Variables & Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+// Variables
+
+import UIKit
+
+var str = "Hello, playground"
+var name = "Michael"
+
+var fullName = name + " Bardwell-Scott"
+fullName
+
+var number = 26
+
+number + 5
+
+// Constants
+let language = "Swift"
+
+// Naming Conventions
+
+let programmingLanguage = "Objective-C"
+
+// Disallowed Characters
+// Cannot start with a number
+// No white space characters
+// No special characters
+// No mathmatical symbols
+
---
+++ Swift Basics/Types.playground/Contents.swift
@@ -0,0 +1,88 @@
+// Create address
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charolotte"
+let street = "West Blvd."
+let buildingNumber = 222
+
+let address = country + ", " + state + ", " + city
+
+// String Interpoloation
+
+let interpolatedAddress = "\(country), \(state), \(city)"
+
+print("You live in \(interpolatedAddress)")
+
+
+/*
+ -----------
+ Integers
+ -----------
+ */
+
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014 // Int
+
+/*
+ ------------
+ Floating Point Numbers
+ ------------
+ */
+
+var version = 4.0 // Double
+
+
+/*
+ -----------
+ Boolean
+ -----------
+ */
+
+let swiftIsAwesome = true
+
+/*
+ -----------
+ Type Safety
+ -----------
+ */
+
+var someString = ""
+// someString = 12.0
+
+let bestPlayer: String = "Michael Jordan"
+let averagePointsPerGame: Double = 30.1
+let yearOfRetirement: Int = 2003
+let hallOfFame: Bool = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
